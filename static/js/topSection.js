ebf815f142af41fbda4e3a8f830d2fa31115e491
myashavanth <yashavantham143@gmail.com>
2024-08-13 21:18:17 +0530

    added gallery page for laptop screan

---
+++ static/js/topSection.js
@@ -0,0 +1,28 @@
+getinTouch = document.getElementById("tsGetinTouchToggleClassBtn");
+FormDataContiner = document.querySelector(".tsFormContainer");
+
+function checkScreenWidth() {
+  if (window.innerWidth > 1024) {
+    FormDataContiner.classList.add("active");
+    getinTouch.classList.add("active");
+    getinTouch.innerHTML = "Close";
+  } else {
+    FormDataContiner.classList.remove("active");
+    getinTouch.classList.remove("active");
+    getinTouch.innerHTML = "Get in Touch";
+  }
+}
+
+checkScreenWidth();
+
+window.addEventListener("resize", checkScreenWidth);
+
+getinTouch.addEventListener("click", () => {
+  FormDataContiner.classList.toggle("active");
+  getinTouch.classList.toggle("active");
+  if (FormDataContiner.classList.contains("active")) {
+    getinTouch.innerHTML = "Close";
+  } else {
+    getinTouch.innerHTML = "Get in Touch";
+  }
+});
